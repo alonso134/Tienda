88c745bf3c0a73c04734062411b679fe69464dc8
a <20220664@ricaldone.edu.sv>
2024-07-12 08:17:02 -0600

    en proceso

---
+++ api/services/public/cliente.php
@@ -0,0 +1,213 @@
+<?php
+// Se incluye la clase del modelo.
+require_once('../../models/data/cliente_data.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $cliente = new ClienteData;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'session' => 0, 'recaptcha' => 0, 'message' => null, 'error' => null, 'exception' => null, 'username' => null);
+    // Se verifica si existe una sesión iniciada como cliente para realizar las acciones correspondientes.
+    if (isset($_SESSION['idCliente'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un cliente ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'getUser':
+                if (isset($_SESSION['correoCliente'])) {
+                    $result['status'] = 1;
+                    $result['username'] = $_SESSION['correoCliente'];
+                    $result['name'] = $cliente->readOneCorreo($_SESSION['correoCliente']);
+                } else {
+                    $result['error'] = 'Correo de usuario indefinido';
+                    $result['name'] ='No se pudo obtener el usuario';
+                }
+                break;
+            case 'logOut':
+                if (session_destroy()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sesión eliminada correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al cerrar la sesión';
+                }
+                break;
+            case 'readProfile':
+                if ($result['dataset'] = $cliente->readProfile()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'Ocurrió un problema al leer el perfil';
+                }
+                break;
+                case 'editProfile':
+                    $_POST = Validator::validateForm($_POST);
+                    if (
+                        !$cliente->setNombre($_POST['nombreCliente']) or
+                        !$cliente->setApellido($_POST['apellidoCliente']) or
+                        !$cliente->setCorreo($_POST['correoCliente']) or
+                        !$cliente->setDUI($_POST['duiCliente']) or
+                        !$cliente->setNacimiento($_POST['nacimientoCliente']) or
+                        !$cliente->setTelefono($_POST['telefonoCliente']) 
+                    ) {
+                        $result['error'] = $cliente->getDataError();
+                    } elseif ($cliente->editProfile()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Perfil modificado correctamente';
+                    } else {
+                        $result['error'] = 'Ocurrió un problema al modificar el perfil';
+                    }
+                    break;
+                case 'changePassword':
+                    $_POST = Validator::validateForm($_POST);
+                    if (!$administrador->checkPassword($_POST['claveActual'])) {
+                        $result['error'] = 'Contraseña actual incorrecta';
+                    } elseif ($_POST['claveNueva'] != $_POST['confirmarClave']) {
+                        $result['error'] = 'Confirmación de contraseña diferente';
+                    } elseif (!$administrador->setClave($_POST['claveNueva'])) {
+                        $result['error'] = $administrador->getDataError();
+                    } elseif ($administrador->changePassword()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Contraseña cambiada correctamente';
+                    } else {
+                        $result['error'] = 'Ocurrió un problema al cambiar la contraseña';
+                    }
+                    break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        // Se compara la acción a realizar cuando el cliente no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'signUp':
+                $_POST = Validator::validateForm($_POST);
+                /* Se establece la clave secreta para el reCAPTCHA de acuerdo con la cuenta de Google.
+                $secretKey = '6LdBzLQUAAAAAL6oP4xpgMao-SmEkmRCpoLBLri-';
+                // Se establece la dirección IP del servidor.
+                $ip = $_SERVER['REMOTE_ADDR'];
+                // Se establecen los datos del raCAPTCHA.
+                $data = array('secret' => $secretKey, 'response' => $_POST['gRecaptchaResponse'], 'remoteip' => $ip);
+                // Se establecen las opciones del reCAPTCHA.
+                $options = array(
+                    'http' => array('header' => 'Content-type: application/x-www-form-urlencoded\r\n', 'method' => 'POST', 'content' => http_build_query($data)),
+                    'ssl' => array('verify_peer' => false, 'verify_peer_name' => false)
+                );
+
+                $url = 'https://www.google.com/recaptcha/api/siteverify';
+                $context = stream_context_create($options);
+                $response = file_get_contents($url, false, $context);
+                $captcha = json_decode($response, true);
+
+                if (!$captcha['success']) {
+                    $result['recaptcha'] = 1;
+                    $result['error'] = 'No eres humano';*/
+                if (!isset($_POST['condicion'])) {
+                    $result['error'] = 'Debe marcar la aceptación de términos y condiciones';
+                } elseif (
+                    !$cliente->setNombre($_POST['nombreCliente']) or
+                    !$cliente->setApellido($_POST['apellidoCliente']) or
+                    !$cliente->setCorreo($_POST['correoCliente']) or
+                    !$cliente->setDireccion($_POST['direccionCliente']) or
+                    !$cliente->setDUI($_POST['duiCliente']) or
+                    !$cliente->setNacimiento($_POST['nacimientoCliente']) or
+                    !$cliente->setTelefono($_POST['telefonoCliente']) or
+                    !$cliente->setClave($_POST['claveCliente'])
+                ) {
+                    $result['error'] = $cliente->getDataError();
+                } elseif ($_POST['claveCliente'] != $_POST['confirmarClave']) {
+                    $result['error'] = 'Contraseñas diferentes';
+                } elseif ($cliente->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Cuenta registrada correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al registrar la cuenta';
+                }
+                break;
+                case 'signUpMovil':
+                    $_POST = Validator::validateForm($_POST);
+                if (
+                        !$cliente->setNombre($_POST['nombreCliente']) or
+                        !$cliente->setApellido($_POST['apellidoCliente']) or
+                        !$cliente->setCorreo($_POST['correoCliente']) or
+                        !$cliente->setDireccion($_POST['direccionCliente']) or
+                        !$cliente->setDUI($_POST['duiCliente']) or
+                        !$cliente->setNacimiento($_POST['nacimientoCliente']) or
+                        !$cliente->setTelefono($_POST['telefonoCliente']) or
+                        !$cliente->setClave($_POST['claveCliente'])
+                    ) {
+                        $result['error'] = $cliente->getDataError();
+                    } elseif ($_POST['claveCliente'] != $_POST['confirmarClave']) {
+                        $result['error'] = 'Contraseñas diferentes';
+                    } elseif ($cliente->createRow()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Cuenta registrada correctamente';
+                    } else {
+                        $result['error'] = 'Ocurrió un problema al registrar la cuenta';
+                    }
+                    break;
+            case 'logIn':
+                $_POST = Validator::validateForm($_POST);
+                if (!$cliente->checkUser($_POST['correo'], $_POST['clave'])) {
+                    $result['error'] = 'Datos incorrectos';
+                } elseif ($cliente->checkStatus()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Autenticación correcta';
+                } else {
+                    $result['error'] = 'La cuenta ha sido desactivada';
+                }
+                break;
+            case 'readProfile':
+                if ($result['dataset'] = $administrador->readProfile()) {
+                    $result['status'] = 1;
+                } else {
+                    $result['error'] = 'Ocurrió un problema al leer el perfil';
+                }
+                break;
+            case 'editProfile':
+                $_POST = Validator::validateForm($_POST);
+                if (
+                    !$cliente->setNombre($_POST['nombreCliente']) or
+                    !$cliente->setApellido($_POST['apellidoCliente']) or
+                    !$cliente->setCorreo($_POST['correoCliente']) or
+                    !$cliente->setDireccion($_POST['direccionCliente']) or
+                    !$cliente->setDUI($_POST['duiCliente']) or
+                    !$cliente->setNacimiento($_POST['nacimientoCliente']) or
+                    !$cliente->setTelefono($_POST['telefonoCliente'])
+                ) {
+                    $result['error'] = $administrador->getDataError();
+                } elseif ($administrador->editProfile()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Perfil modificado correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al modificar el perfil';
+                }
+                break;
+            case 'changePassword':
+                $_POST = Validator::validateForm($_POST);
+                if (!$administrador->checkPassword($_POST['claveActual'])) {
+                    $result['error'] = 'Contraseña actual incorrecta';
+                } elseif ($_POST['claveNueva'] != $_POST['confirmarClave']) {
+                    $result['error'] = 'Confirmación de contraseña diferente';
+                } elseif (!$administrador->setClave($_POST['claveNueva'])) {
+                    $result['error'] = $administrador->getDataError();
+                } elseif ($administrador->changePassword()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Contraseña cambiada correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al cambiar la contraseña';
+                }
+                break;
+
+            default:
+                $result['error'] = 'Acción no disponible fuera de la sesión';
+        }
+    }
+    // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+    $result['exception'] = Database::getException();
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('Content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
---
+++ controllers/utils/admin.js
@@ -0,0 +1,127 @@
+/*
+*   Controlador de uso general en las páginas web del sitio privado.
+*   Sirve para manejar la plantilla del encabezado y pie del documento.
+*/
+
+// Constante para completar la ruta de la API.
+const USER_API = 'services/admin/administrador.php';
+// Constante para establecer el elemento del contenido principal.
+const MAIN = document.querySelector('main');
+MAIN.style.paddingTop = '75px';
+MAIN.style.paddingBottom = '100px';
+MAIN.classList.add('container');
+// Se establece el título de la página web.
+document.querySelector('title').textContent = 'booksadre - Dashboard';
+// Constante para establecer el elemento del título principal.
+const MAIN_TITLE = document.getElementById('mainTitle');
+MAIN_TITLE.classList.add('text-center', 'py-3');
+
+/*  Función asíncrona para cargar el encabezado y pie del documento.
+*   Parámetros: ninguno.
+*   Retorno: ninguno.
+*/
+const loadTemplate = async () => {
+    // Petición para obtener en nombre del usuario que ha iniciado sesión.
+    const DATA = await fetchData(USER_API, 'getUser');
+    // Se verifica si el usuario está autenticado, de lo contrario se envía a iniciar sesión.
+    if (DATA.session) {
+        // Se comprueba si existe un alias definido para el usuario, de lo contrario se muestra un mensaje con la excepción.
+        if (DATA.status) {
+            // Se agrega el encabezado de la página web antes del contenido principal.
+            MAIN.insertAdjacentHTML('beforebegin', `
+                <header>
+                <div class="menu ">
+                <nav class="navbar bg-body-tertiary fixed-top ">
+                  <div class="container-fluid ">
+               
+                    <a class="navbar-brand"><img src="../../resources/img/logo.png" alt="Logo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                      <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Administrador</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                      </div>
+                      <div class="offcanvas-body">
+                        <div class="circle-container">
+                          <div class="circle">
+                            <img src="https://cdn-icons-png.flaticon.com/512/456/456283.png" alt="Imagen en un círculo">
+                          </div>
+              
+                        </div>          
+              
+                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                          
+              
+                          <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="../../views/admin/Inicio.html"><i class='bx bxs-home-alt-2' ></i> Inicio</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="../../views/admin/Categorias.html"><i class='bx bxs-book-bookmark' ></i> Categorías</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" ria-current="page" href="../../views/admin/Productos.html"><i class='bx bxs-pie-chart-alt-2'></i> Productos</a>
+                          </li>
+                          <li class="nav-item ">
+                          <a class="nav-link active" aria-current="page" href="../../views/admin/Pedidos.html"><i class='bx bxs-report'></i> Pedidos</a>
+                        </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="../../views/admin/Clientes.html"><i class='bx bxs-user'></i> Clientes</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="../../views/admin/Administrador.html"><i class='bx bxs-user-plus' ></i> Administradores</a>
+                          </li>
+                          <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Cuenta: <b>${DATA.username}</b></a>
+                              <ul class="dropdown-menu">
+                                  <li><a class="dropdown-item" href="profile.html">Editar perfil</a></li>
+                                  <li><hr class="dropdown-divider"></li>
+                                  <li><a class="dropdown-item" href="#" onclick="logOut()">Cerrar sesión</a></li>
+                              </ul>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+              
+              
+                </header>
+            `);
+        
+        } else {
+            sweetAlert(3, DATA.error, false, 'index.html');
+        }
+    } else {
+        // Se comprueba si la página web es la principal, de lo contrario se direcciona a iniciar sesión.
+        if (location.pathname.endsWith('index.html')) {
+            // Se agrega el encabezado de la página web antes del contenido principal.
+            MAIN.insertAdjacentHTML('beforebegin', `
+                <header>
+                    <nav class="navbar fixed-top bg-body-tertiary">
+                        <div class="container">
+                            <a class="navbar-brand" href="index.html">
+                                <img src="../../resources/img/logo.png" alt="inventory" width="50">
+                            </a>
+                        </div>
+                    </nav>
+                </header>
+            `);
+            // Se agrega el pie de la página web después del contenido principal.
+            MAIN.insertAdjacentHTML('afterend', `
+                <footer>
+                    <nav class="navbar fixed-bottom bg-body-tertiary">
+                        <div class="container">
+                            <p><a class="nav-link" href="https://github.com/milesssssssss/Tienda" target="_blank"><i class="bi bi-github"></i> booksadre</a></p>
+                            <p><i class="bi bi-envelope-fill"></i> booksadre23@gmail.com</p>
+                        </div>
+                    </nav>
+                </footer>
+            `);
+        } else {
+            location.href = 'index.html';
+        }
+    }
+}
